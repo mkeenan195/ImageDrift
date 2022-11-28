7f3c701ff546e6b8412bd7cf83e91b7214d249f5
mkeenan195 <72765234+mkeenan195@users.noreply.github.com>
2022-11-28 15:34:01 -0500

    added style transfer function

---
+++ style_transfer.py
@@ -0,0 +1,273 @@
+# Code adapted from PyTorch Style Transfer Tutorial
+# https://pytorch.org/tutorials/advanced/neural_style_tutorial.html
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision.models as models
+from torchvision.utils import save_image
+from PIL import Image
+import matplotlib.pyplot as plt
+from dataloader import ImagingDataset
+import os
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def get_image(dataset, img_name="202002261456-9of48-f3600"):
+    idx = dataset.image_names.index(img_name)
+    img = dataset[idx].unsqueeze(0)
+    return img
+
+
+def plot_image(img, title=None, output_file="output.jpg"):
+    if len(img.shape) == 4:
+        img = img[0]
+    img = img.permute(1, 2, 0).detach().cpu().numpy()
+    fig, ax = plt.subplots()
+    ax.imshow(img, cmap="gray", vmin=0, vmax=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    if title:
+        ax.set_title(title)
+    plt.savefig(output_file)
+
+
+class ContentLoss(nn.Module):
+    def __init__(
+        self,
+        target,
+    ):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+
+def gram_matrix(input):
+    a, b, c, d = input.size()
+    features = input.view(a * b, c * d)
+    G = torch.mm(features, features.t())
+    return G.div(a * b * c * d)
+
+
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
+# create a module to normalize input image so we can easily put it in a
+# nn.Sequential
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
+
+# desired depth layers to compute style/content losses :
+content_layers_default = ["conv_4"]
+style_layers_default = ["conv_1", "conv_2", "conv_3", "conv_4", "conv_5"]
+
+
+def get_style_model_and_losses(
+    cnn,
+    normalization_mean,
+    normalization_std,
+    style_img,
+    content_img,
+    content_layers=content_layers_default,
+    style_layers=style_layers_default,
+):
+    # normalization module
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
+
+    # just in order to have an iterable access to or list of content/syle
+    # losses
+    content_losses = []
+    style_losses = []
+
+    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
+    model = nn.Sequential(normalization)
+
+    i = 0  # increment every time we see a conv
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = "conv_{}".format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = "relu_{}".format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place
+            # ones here.
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = "pool_{}".format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = "bn_{}".format(i)
+        else:
+            raise RuntimeError(
+                "Unrecognized layer: {}".format(layer.__class__.__name__)
+            )
+
+        model.add_module(name, layer)
+
+        if name in content_layers:
+            # add content loss:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        if name in style_layers:
+            # add style loss:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    # now we trim off the layers after the last content and style losses
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[: (i + 1)]
+
+    return model, style_losses, content_losses
+
+
+def get_input_optimizer(input_img):
+    # this line to show that input is a parameter that requires a gradient
+    optimizer = optim.LBFGS([input_img])
+    return optimizer
+
+
+def run_style_transfer(
+    cnn,
+    normalization_mean,
+    normalization_std,
+    content_img,
+    style_img,
+    input_img,
+    num_steps=300,
+    style_weight=1000000,
+    content_weight=1,
+):
+    """Run the style transfer."""
+    print("Building the style transfer model..")
+    model, style_losses, content_losses = get_style_model_and_losses(
+        cnn, normalization_mean, normalization_std, style_img, content_img
+    )
+
+    # We want to optimize the input and not the model parameters so we
+    # update all the requires_grad fields accordingly
+    input_img.requires_grad_(True)
+    model.requires_grad_(False)
+
+    optimizer = get_input_optimizer(input_img)
+
+    print("Optimizing..")
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            # correct the values of updated input image
+            with torch.no_grad():
+                input_img.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print(
+                    "Style Loss : {:4f} Content Loss: {:4f}".format(
+                        style_score.item(), content_score.item()
+                    )
+                )
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    with torch.no_grad():
+        input_img.clamp_(0, 1)
+
+    return input_img
+
+
+def create_style_augmented_images(
+    style_img_name, content_img_names, image_dir, output_dir, num_steps=300
+):
+    dataset = ImagingDataset(
+        image_dir, start_date="0101", end_date="1231", transform=None
+    )
+    style_img = get_image(dataset, img_name=style_img_name)
+    os.mkdir(os.path.join(output_dir, style_img_name))
+    cnn = (
+        models.vgg19(weights=models.VGG19_Weights.IMAGENET1K_V1)
+        .features.to(device)
+        .eval()
+    )
+    cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+    cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+    for content_img_name in content_img_names:
+        content_img = get_image(dataset, img_name=content_img_name)
+        input_img = content_img.clone()
+        output_img = run_style_transfer(
+            cnn,
+            cnn_normalization_mean,
+            cnn_normalization_std,
+            content_img,
+            style_img,
+            input_img,
+            num_steps=num_steps,
+        )
+        output_fp = os.path.join(output_dir, style_img_name, content_img_name + ".jpg")
+        save_image(output_img[0], fp=output_fp)
+
+
+if __name__ == "__main__":
+
+    style_img_name = "20200814_clip_16_0703_img_115"
+    content_img_names = ["20210110_clip_28_1230_img_5",
+                        "20200810_clip_11_1452_img_115"]
+    create_style_augmented_images(
+        style_img_name=style_img_name,
+        content_img_names=content_img_names,
+        image_dir="datasets/thermal/images",
+        output_dir="temp",
+        num_steps=200,
+    )
